8e985b7cfb2df19b47243db1600784a6ce0757a2
Andres Romero <andres.romero.90@hotmail.com>
2022-06-01 21:43:42 -0300

    Resolucion en TS

--- typescript/BolsaDePapaNoel.ts
+++ typescript/BolsaDePapaNoel.ts
@@ -1,28 +1,40 @@
-
 export interface Regalo {
-    name: string,
-    peso: number,
+  name: string;
+  peso: number;
 }
 
 class BolsaDePapaNoel {
+  private static PESO_MAXIMO = 5000;
+  private _peso = 0;
+  private _regalos: Regalo[] = [];
 
-    regalos: Regalo[] = []
-
-    agregarRegalo(regalo: Regalo) {
-        throw Error("Implement Method")
+  agregarRegalo(regalo: Regalo) {
+    if (regalo.peso > BolsaDePapaNoel.PESO_MAXIMO) {
+      throw Error("El regalo no puede pesar más de 5 kg");
     }
-
-    yaExiste(regalo: Regalo): boolean {
-        throw Error("Implement Method")
+    if (this.yaExiste(regalo)) {
+      throw Error("El regalo ya fue agregado");
     }
+    this._regalos.push(regalo);
+    this._peso += regalo.peso;
+  }
 
-    pesoActual(): number {
-        throw Error("Implement Method")
-    }
+  yaExiste(regalo: Regalo): boolean {
+    return this._regalos.some(
+      (regaloBolsa: Regalo) =>
+        regaloBolsa.name === regalo.name && regaloBolsa.peso === regalo.peso
+    );
+  }
 
-    regaloMasPesado(): Regalo | undefined {
-        throw Error("Implement Method")
-    }
+  pesoActual(): number {
+    return this._peso;
+  }
+
+  regaloMasPesado(): Regalo | undefined {
+    return this._regalos.reduce((prev, cur) =>
+      prev.peso > cur.peso ? prev : cur
+    );
+  }
 }
 
-export default BolsaDePapaNoel
+export default BolsaDePapaNoel;
