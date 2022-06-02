4b733eafc1c1f3a39d00fffcc6281a9eac232809
Andres Romero <andres.romero.90@hotmail.com>
2022-06-01 21:43:29 -0300

    Resolucion en php

--- php/BolsaDePapaNoel.php
+++ php/BolsaDePapaNoel.php
@@ -1,23 +1,49 @@
 <?php
 
-class Regalo {
-    public function __construct(public string $name, public int $peso) {}
+class Regalo
+{
+  public function __construct(public string $name, public int $peso)
+  {
+  }
 }
 
-class BolsaDePapaNoel {
-    public function agregarRegalo(Regalo $regalo) {
-        throw new Exception("To be implemented");
-    }
+class BolsaDePapaNoel
+{
+  public static int $PESO_MAXIMO = 5000;
+  private int $_peso;
+  private array $_regalos;
 
-    public function yaExiste(Regalo $regalo) {
-        throw new Exception("To be implemented");
-    }
+  public function __construct()
+  {
+    $this->_regalos = array();
+    $this->_peso = 0;
+  }
 
-    public function pesoActual() {
-        throw new Exception("To be implemented");
+  public function agregarRegalo(Regalo $regalo): void
+  {
+    if ($regalo->peso > BolsaDePapaNoel::$PESO_MAXIMO || ($regalo->peso + $this->_peso) > BolsaDePapaNoel::$PESO_MAXIMO) {
+      throw new Exception("La bolsa no puede contener mas de 5000 gramos");
     }
+    if ($this->yaExiste($regalo)) {
+      throw new Exception("El regalo ya fue agregado");
+    }
+    array_push($this->_regalos, $regalo);
+    $this->_peso += $regalo->peso;
+  }
 
-    public function regaloMasPesado() {
-        throw new Exception("To be implemented");
-    }
+  public function yaExiste(Regalo $regalo): bool
+  {
+    return in_array($regalo, $this->_regalos);
+  }
+
+  public function pesoActual(): int
+  {
+    return $this->_peso;
+  }
+
+  public function regaloMasPesado(): string
+  {
+    $regaloMasPesado = array_reduce($this->_regalos, fn ($a, $b) => $a->peso > $b->peso ? $a : $b, new Regalo('', 0));
+    return $regaloMasPesado->name;
+  }
 }
